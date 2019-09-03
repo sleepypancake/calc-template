b2d4258ad3d06836597eac2e516c6ef082f81da3
Danil Sergeevich <mrthefirst@gmail.com>
2019-09-03 12:07:15 +0300

    Поправил конфиг для нормальной работы swiper в IE

--- webpack.config.js
+++ webpack.config.js
@@ -76,9 +76,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: [/node_modules/],
+        exclude: /node_modules[\/\\](?!(swiper|dom7)[\/\\])/,
         use: {
           loader: "babel-loader",
+          options: { presets: ['@babel/preset-env' ] }
         }
       },
       {
